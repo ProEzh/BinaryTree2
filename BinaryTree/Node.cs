dc6f39b7b863c519ae0bd0d9f024d88ebdbe17b4
simplydesigner <simplydesigner@yandex.ru>
2014-11-25 10:57:56 -0800

    Refactoring and Fixing bug

--- BinaryTree/Node.cs
+++ BinaryTree/Node.cs
@@ -1,82 +1,95 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System;
 
 namespace BinaryTree
 {
-    public class Node
-    {
-        private int value_;
-        public int Value
-        {
-            get
-            {
-                return value_;
-            }
-            set {
-                value_ = value;
-            }
-        }
-        
-        private Node leftNode_;
-        public Node LeftNode
-        {
-            get
-            {
-                return leftNode_;
-            }
-            set
-            {
-                leftNode_ = value;
-            }
-        }
-        
-        private Node rightNode_;
-        public Node RightNode
-        {
-            get
-            {
-                return rightNode_;
-            }
-            set
-            {
-                rightNode_ = value;
-            }
-        }
-        
-        public Node(int value) {
-            value_ = value;
-            leftNode_ = null;
-            rightNode_ = null;
-        }
-        
-        public void Print()
-        {
-        }
-        
-        public bool HasRightNode()
-        {
-            return rightNode_ != null;
-        }
-        
-        public bool HasLeftNode()
-        {
-            return leftNode_ != null;
-        }
-        
-        public Node InsertChildWithValue(int value)
-        {
-            if (value > value_) {
-                Node child = new Node(value);
-                this.RightNode = child;
-                return child;
-            } else if (value < value_) {
-                Node child = new Node(value);
-                this.LeftNode = child;
-                return child;
-            } else {
-                return this;
-            }
-        }
-    }
+	public class Node
+	{
+		private int value_;
+		public int Value
+		{
+			get
+			{
+				return value_;
+			}
+			set {
+				value_ = value;
+			}
+		}
+
+		private Node leftNode_;
+		public Node LeftNode
+		{
+			get
+			{
+				return leftNode_;
+			}
+			set
+			{
+				leftNode_ = value;
+			}
+		}
+
+		private Node rightNode_;
+		public Node RightNode
+		{
+			get
+			{
+				return rightNode_;
+			}
+			set
+			{
+				rightNode_ = value;
+			}
+		}
+
+		public Node(int value) {
+			value_ = value;
+			leftNode_ = null;
+			rightNode_ = null;
+		}
+
+		public void Print(int level = 0)
+		{
+			if (this.HasRightNode ()) {
+				this.RightNode.Print (level + 1);
+			}
+
+			for (uint i = 0; i < level; ++i) {
+				Console.Write("-");
+			}
+			Console.WriteLine("{0:D}", value_);
+
+			if (this.HasLeftNode ()) {
+				this.LeftNode.Print (level + 1);
+			}
+		}
+
+		public bool HasRightNode()
+		{
+			return rightNode_ != null;
+		}
+
+		public bool HasLeftNode()
+		{
+			return leftNode_ != null;
+		}
+
+		public Node InsertChildWithValue(int value)
+		{
+			if (value > value_) {
+				Node child = new Node(value);
+				this.RightNode = child;
+				return child;
+			} else if (value < value_) {
+				Node child = new Node(value);
+				this.LeftNode = child;
+				return child;
+			} else {
+				return this;
+			}
+		}
+	}
 }
--- BinaryTree/Tree.cs
+++ BinaryTree/Tree.cs
@@ -5,111 +5,98 @@ using System.Text;
 
 namespace BinaryTree
 {
-    class Tree
-    {
-        private Node rootNode_;
-        public Node RootNode
-        {
-            get
-            {
-                return rootNode_;
-            }
-            set
-            {
-                rootNode_ = value;
-            }
-        }
-        
-        
-        public Tree()
-        {
-            rootNode_ = null;
-        }
-        
-        public bool IsEmpty() {
-            return rootNode_ == null;
-        }
-        
-        public Node SetRootNodeWithValue(int value) {
-            rootNode_ = new Node (value);
-            return rootNode_;
-        }
-        
-        public Node InsertNodeWithValue(int value)
-        {
-            if (this.IsEmpty ()) {
-                return this.SetRootNodeWithValue (value);
-            }
-            
-            Node currentNode = rootNode_;
-            for (;;)
-            {
-                if (value > currentNode.Value)
-                {
-                    if ( currentNode.HasRightNode() )
-                    {
-                        currentNode = currentNode.RightNode;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-                if (value < currentNode.Value) {
-                    if (currentNode.HasLeftNode ()) {
-                        currentNode = currentNode.LeftNode;
-                    } 
-                    else {
-                        break;
-                    }
-                }
-                else {
-                    break;
-                }
-            }
-            
-            return currentNode.InsertChildWithValue(value);
-        }
-        
-        public void Print()
-        {
-            if (this.IsEmpty())
-                Console.WriteLine("Дерева нет");
-            Node currentNode = rootNode_;
-            for (; ; )
-            {
-                if (currentNode.RightNode != null)
-                {
-                    currentNode = currentNode.RightNode;
-                    Console.Write("-");
-                    currentNode.Print();
-                }
-                else 
-                {
-                    Console.Write(currentNode.Value);
-                    break;
-                }
-            }
-        }
-        
-        private static void Main()
-        {
-            Tree tree = new Tree();
-            tree.InsertNodeWithValue(3);
-            tree.InsertNodeWithValue(2);
-            tree.InsertNodeWithValue(4);
-            tree.InsertNodeWithValue(5);
-            tree.InsertNodeWithValue(6);
-            tree.InsertNodeWithValue(7);
-            tree.InsertNodeWithValue(8);
-            tree.InsertNodeWithValue(9);
-            tree.InsertNodeWithValue(10);
-            tree.InsertNodeWithValue(11);
-            tree.InsertNodeWithValue(12);
-            tree.InsertNodeWithValue(13);
-            tree.InsertNodeWithValue(14);
-            tree.Print();
-            //          TODO OUTPUT: 1
-        }
-    }
+	class Tree
+	{
+		private Node rootNode_;
+		public Node RootNode
+		{
+			get
+			{
+				return rootNode_;
+			}
+			set
+			{
+				rootNode_ = value;
+			}
+		}
+
+
+		public Tree()
+		{
+			rootNode_ = null;
+		}
+
+		public bool IsEmpty() {
+			return rootNode_ == null;
+		}
+
+		public Node SetRootNodeWithValue(int value) {
+			rootNode_ = new Node (value);
+			return rootNode_;
+		}
+
+		public Node InsertNodeWithValue(int value)
+		{
+			if (this.IsEmpty ()) {
+				return this.SetRootNodeWithValue (value);
+			}
+
+			Node currentNode = rootNode_;
+			for (;;)
+			{
+				if (value > currentNode.Value)
+				{
+					if ( currentNode.HasRightNode() )
+					{
+						currentNode = currentNode.RightNode;
+						continue;
+					}
+					else
+					{
+						break;
+					}
+				}
+				else if (value < currentNode.Value) {
+					if (currentNode.HasLeftNode ()) {
+						currentNode = currentNode.LeftNode;
+						continue;
+					}
+					else {
+						break;
+					}
+				}
+				else {
+					break;
+				}
+			}
+
+			return currentNode.InsertChildWithValue(value);
+		}
+
+		public void Print()
+		{
+			if (!this.IsEmpty ()) {
+				this.RootNode.Print();
+			}
+		}
+
+		private static void Main()
+		{
+			Tree tree = new Tree();
+			tree.InsertNodeWithValue(3);
+			tree.InsertNodeWithValue(2);
+			tree.InsertNodeWithValue(4);
+			tree.InsertNodeWithValue(5);
+			tree.InsertNodeWithValue(6);
+			tree.InsertNodeWithValue(7);
+			tree.InsertNodeWithValue(8);
+			tree.InsertNodeWithValue(9);
+			tree.InsertNodeWithValue(10);
+			tree.InsertNodeWithValue(11);
+			tree.InsertNodeWithValue(12);
+			tree.InsertNodeWithValue(13);
+			tree.InsertNodeWithValue(14);
+			tree.Print();
+		}
+	}
 }
